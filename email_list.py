9d06f45c475f7ff351ce70e83fb0ff1a45cda1d7
Erkan Cetinkaya <33022765+erkcet@users.noreply.github.com>
2022-03-24 13:07:51 -0400

    update

--- email_list.py
+++ email_list.py
@@ -16,4 +16,4 @@ def email_list(domains):
 			emails.append(email)
 	return(emails)
 
-print(email_list({"gmail.com": ["clark.kent", "diana.prince", "peter.parker"], "yahoo.com": ["barbara.gordon", "jean.grey"], "hotmail.com": ["bruce.wayne"]}))
+print(email_list({"gmail.com": ["clark.kent", "diana.prince", "peter.parker", "tim.cook"], "yahoo.com": ["barbara.gordon", "jean.grey"], "hotmail.com": ["bruce.wayne"]}))
